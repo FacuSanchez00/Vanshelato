a6a0a8796ca80abc2e6847ba888d729dcc003e9d
FacuSanchez00 <facundofsanchezp@gmail.com>
2022-10-25 19:18:50 -0300

    agregando logica de js

---
+++ JS/hacerpedido.js
@@ -0,0 +1,24 @@
+const direccion = prompt("Ingrese direccion");
+console.log(direccion);
+const tamano = prompt("Elija entre 1/4 KG, 1/2 KG o 1 KG");
+console.log(tamano);
+const gustos = prompt(
+  "Elija 3 de: Frutilla, Vainilla, Chocolate, Dulce de Leche, Tiramisu"
+);
+
+if (direccion && tamano && gustos) {
+  alert(
+    "Se está preparando el pedido para " +
+      direccion +
+      " con tamaño de " +
+      tamano +
+      " y gustos " +
+      gustos
+  );
+} else if (!direccion) {
+  alert("ingrese una dirección");
+} else if (!tamano) {
+  alert("ingrese un tamaño");
+} else if (!gustos) {
+  alert("ingrese gustos");
+}
