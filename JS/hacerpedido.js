427cbe5c66d3448e550b5cccd34215fa57b3c4f8
FacuSanchez00 <facundofsanchezp@gmail.com>
2022-11-29 20:12:45 -0300

    agregando JSON y DOM

--- JS/hacerpedido.js
+++ JS/hacerpedido.js
@@ -1,24 +1,33 @@
-const direccion = prompt("Ingrese direccion");
-console.log(direccion);
-const tamano = prompt("Elija entre 1/4 KG, 1/2 KG o 1 KG");
-console.log(tamano);
-const gustos = prompt(
-  "Elija 3 de: Frutilla, Vainilla, Chocolate, Dulce de Leche, Tiramisu"
-);
+// const direccion = prompt("Ingrese direccion");
+// console.log(direccion);
+// const tamano = prompt("Elija entre 1/4 KG, 1/2 KG o 1 KG");
+// console.log(tamano);
+// const gustos = prompt(
+//   "Elija 3 de: Frutilla, Vainilla, Chocolate, Dulce de Leche, Tiramisu"
+// );
 
-if (direccion && tamano && gustos) {
-  alert(
-    "Se está preparando el pedido para " +
-      direccion +
-      " con tamaño de " +
-      tamano +
-      " y gustos " +
-      gustos
-  );
-} else if (!direccion) {
-  alert("ingrese una dirección");
-} else if (!tamano) {
-  alert("ingrese un tamaño");
-} else if (!gustos) {
-  alert("ingrese gustos");
-}
+// if (direccion && tamano && gustos) {
+//   alert(
+//     "Se está preparando el pedido para " +
+//       direccion +
+//       " con tamaño de " +
+//       tamano +
+//       " y gustos " +
+//       gustos
+//   );
+// } else if (!direccion) {
+//   alert("ingrese una dirección");
+// } else if (!tamano) {
+//   alert("ingrese un tamaño");
+// } else if (!gustos) {
+//   alert("ingrese gustos");
+// }
+
+//Guardar datos del usuario
+
+const usuario = {
+  nombre: document.getElementById("nombreApellido"),
+  email: document.getElementById("email"),
+};
+
+localStorage.setItem("user", JSON.stringify(usuario));
